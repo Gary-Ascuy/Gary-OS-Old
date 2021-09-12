2fc794edb274d8a26d4b9b25f7b893b603a8f662
Gary Ascuy <gary.ascuy@gmail.com>
2021-09-12 12:43:20 -0500

    Adding logic for terminal

--- site/components/apps/terminal/Terminal.tsx
+++ site/components/apps/terminal/Terminal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import Window from '../../window/Window';
 
 import style from './Terminal.module.css'
@@ -6,25 +6,53 @@ import style from './Terminal.module.css'
 // { process }: TerminalProps extends AppicationProps
 export default function Terminal() {
   const [value, setValue] = useState('')
+  const [PS1, setPS1] = useState('gary @Gary-MacBook-Pro site %')
+  const [lines, setLines] = useState([
+    'gary @Gary-MacBook-Pro site % docker run -it -p 80:80 nginx',
+    'zsh: command not found: docker',
+    'gary @Gary-MacBook-Pro site % docker-compose build -d',
+    'zsh: command not found: docker-compose',
+    'gary @Gary-MacBook-Pro site %',
+    'gary @Gary-MacBook-Pro site %',
+    'gary @Gary-MacBook-Pro site %',
+  ])
+
+  const addLines = useCallback((...newLines: string[]) => {
+    setLines([...lines, ...newLines])
+  }, [lines])
 
   return (
     <Window title='gary -- -zsh -- 80x24'>
       <div style={{ position: 'relative' }}>
         <div className={style.output}>
-          <div>gary @Gary-MacBook-Pro site % docker run -it -p 80:80 nginx</div>
-          <div>zsh: command not found: docker</div>
-          <div>gary @Gary-MacBook-Pro site % docker-compose build -d</div>
-          <div>zsh: command not found: docker-compose</div>
-          <div>gary @Gary-MacBook-Pro site %</div>
-          <div>gary @Gary-MacBook-Pro site %</div>
-          <div>gary @Gary-MacBook-Pro site %</div>
+          {lines.map((line: string, index: number) => <div key={index}>{line}</div>)}
         </div>
       </div>
 
       <div style={{ position: 'relative' }}>
-        <span className={style.ps1}>gary @Gary-MacBook-Pro site %</span>
+        <span className={style.ps1}>{PS1}</span>
 
-        <textarea className={style.input} value={value} onChange={(event) => setValue(event.target.value)}>
+        <textarea rows={10} className={style.input} value={value} onChange={(event) => {
+          const { value } = event.target
+
+
+          if (/\n/.test(value)) {
+            if (/clear/.test(value)) {
+              setLines([])
+              setValue('')
+              return
+            }
+
+            if (!value.trim()) {
+              addLines(PS1)
+              return
+            }
+
+            const [command] = value.split(' ')
+            addLines(`${PS1} ${value}`, `zsh: command not found: ${command}`)
+            setValue('')
+          } else setValue(value)
+        }}>
         </textarea>
       </div>
     </Window >
