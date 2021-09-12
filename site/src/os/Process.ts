d10d7e1fb47e886d953817e38f95680857878019
Gary Ascuy <gary.ascuy@gmail.com>
2021-09-12 09:29:40 -0500

    Adding gNode process implementation

---
+++ site/src/os/Process.ts
@@ -0,0 +1,67 @@
+// import { Command } from "@garyos/commander"
+
+export const version = 'v1.0.0'
+export const versions = { gnode: version }
+
+export const arch = 'x64'
+export const platform = 'garyos'
+
+export const release = {
+  name: 'gnode',
+  sourceUrl: 'https://github.com/Gary-Ascuy/Gary-OS',
+  headersUrl: 'https://github.com/Gary-Ascuy/Gary-OS'
+}
+
+export function getSystemEnvs(): EnvironmentVariables {
+  return {
+    PATH: 'local',
+    HOME: '/root/gary',
+  }
+}
+
+export interface EnvironmentVariables {
+  [key: string]: string
+}
+
+export function getProcess(localEnvs: EnvironmentVariables) {
+  const env = { ...getSystemEnvs(), ...localEnvs }
+  const argv = ['/Users/gary/.nvm/versions/node/v1.0.0/bin/gnode']
+
+  // Process implementation
+  const process = {
+    version, versions, arch, platform, release,
+    env, argv,
+
+    // functions
+    __starttime: new Date().getTime(),
+    uptime: () => (0.0 + new Date().getTime() - process.__starttime) / 1000.0,
+    exit: (code: number) => console.log(code),
+  }
+  return process
+}
+
+// spawn('setTimeout(() => console.log(process.uptime()), 2000)', console)
+export function spawn(program = '', __console: any) {
+  let process = getProcess({ DB_SYSTEM: 'test' })
+  let __output = __console
+  let console = {
+    log: (...args: any) => __output.log('START', ...args, 'END'),
+  }
+
+  eval(program)
+}
+
+export function gnodeProgram() {
+  //   const program = new Command()
+  //   program.name('explore')
+  //     .option('-d, --debug', 'output extra debugging')
+  //     .option('-s, --small', 'small pizza size')
+  //     .option('-p, --pizza-type <type>', 'flavour of pizza')
+
+  //   program.parse(['/Users/gary/.nvm/versions/node/v15.8.0/bin/node'])
+
+  // <pre style={{ position: 'fixed', top: '60px', left: '10px', color: 'white' }}>
+  //   {program.helpInformation()}
+  //   {program.help()}
+  // </pre>
+}
