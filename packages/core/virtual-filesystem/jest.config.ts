9d0d84dd0413c977ef34d501003fd096dcdf9f15
Gary Ascuy <gary.ascuy@gmail.com>
2021-09-14 20:14:53 -0500

    Updating project settings

--- packages/core/virtual-filesystem/jest.config.ts
+++ packages/core/virtual-filesystem/jest.config.ts
@@ -1,8 +1,17 @@
-import type {Config} from '@jest/types'
+import type { Config } from '@jest/types'
 
 export default async (): Promise<Config.InitialOptions> => {
   return {
+    preset: "ts-jest",
     verbose: true,
+    testEnvironment: 'jsdom',
     modulePathIgnorePatterns: ['lib/'],
+    roots: ['<rootDir>/src'],
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    coveragePathIgnorePatterns: [
+      "/node_modules/"
+    ],
   }
 }
