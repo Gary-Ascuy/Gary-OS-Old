02a039c0f61f1ba64d547fec9bee6edc331522fa
Gary Ascuy <gary.ascuy@gmail.com>
2021-09-18 15:37:14 -0500

    Adding icons updating favicon

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -2,12 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect } from 'react'
 import { useRecoilState } from 'recoil'
+
 import { Desktop } from '../components/core/Desktop'
 
 import { userState } from '../src/recoil/user.atom'
 
 const Home: NextPage = () => {
   const [user, setUser] = useRecoilState(userState)
+
+  // const kernel = useRecoilState(userState)
+  // const [fs, refresh] = useFileSystem()
+  // const [kernel, open] = useKernel()
+  // const test = useInfo()
 
   useEffect(() => {
     // console.log(user)
@@ -18,7 +24,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Gary OS</title>
         <meta name="description" content="gOS - Gary OS is Web Application Profile (Gary Ascuy)" />
-        <link rel="icon" type="image/png" href="/assets/web/favicon_small.png" />
+        <link rel="icon" type="image/png" href="/assets/web/favicon.png" />
       </Head>
 
       <main>
